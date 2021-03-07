d543819a056ce40463baf1637af1a632f5a8c88c
tojhe <jinator.biz@gmail.com>
2021-03-07 23:34:08 +0800

    added waveform display visualization command

--- DeckGUI.cpp
+++ DeckGUI.cpp
@@ -12,7 +12,11 @@
 #include "DeckGUI.h"
 
 //==============================================================================
-DeckGUI::DeckGUI(DJAudioPlayer* _player) : player(_player)
+DeckGUI::DeckGUI(DJAudioPlayer* _player,
+                juce::AudioFormatManager& formatManagerToUse,
+                juce::AudioThumbnailCache& cacheToUse
+                ) : player(_player),
+                    waveformDisplay(formatManagerToUse, cacheToUse)
 {
     // In your constructor, you should add any child components, and
     // initialise any special settings that your component needs.
@@ -24,6 +28,8 @@ DeckGUI::DeckGUI(DJAudioPlayer* _player) : player(_player)
     addAndMakeVisible(volSlider);
     addAndMakeVisible(speedSlider);
     addAndMakeVisible(posSlider);
+
+    addAndMakeVisible(waveformDisplay);
 
     playButton.addListener(this);
     stopButton.addListener(this);
@@ -66,7 +72,7 @@ void DeckGUI::resized()
     // This method is where you should set the bounds of any child
     // components that your component contains..
 
-    double rowH = getHeight() / 6;
+    double rowH = getHeight() / 8;
 
     playButton.setBounds(0, 0, getWidth(), rowH);
     stopButton.setBounds(0, rowH, getWidth(), rowH);
@@ -74,8 +80,10 @@ void DeckGUI::resized()
     volSlider.setBounds(0, rowH * 2, getWidth(), rowH);
     speedSlider.setBounds(0, rowH * 3, getWidth(), rowH);
     posSlider.setBounds(0, rowH * 4, getWidth(), rowH);
+
+    waveformDisplay.setBounds(0, rowH * 5, getWidth(), rowH * 2);
     
-    loadButton.setBounds(0, rowH * 5, getWidth(), rowH);    
+    loadButton.setBounds(0, rowH * 7, getWidth(), rowH);    
 }
 
 void DeckGUI::buttonClicked(juce::Button* button)
@@ -98,6 +106,7 @@ void DeckGUI::buttonClicked(juce::Button* button)
         if (chooser.browseForFileToOpen())
         {
             player->loadURL(juce::URL {chooser.getResult()});
+            waveformDisplay.loadURL(juce::URL {chooser.getResult()});
         }
     }
 }
@@ -136,5 +145,6 @@ void DeckGUI::filesDropped (const StringArray &files, int x, int y)
   if (files.size() == 1)
   {
     player->loadURL(juce::URL{juce::File{files[0]}});
+    waveformDisplay.loadURL(juce::URL{juce::File{files[0]}});
   }
 }
